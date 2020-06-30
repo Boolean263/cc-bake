2cbe08e761fbc11629d7dd5e37cb540e3f551389
David Perry <d.perry@utoronto.ca>
2020-06-30 11:33:12 -0400

    make linter happy

--- tests/test_recipe.py
+++ tests/test_recipe.py
@@ -1,10 +1,13 @@
 from ccbake import Recipe
 
+
 def test_recipe():
     # Explicit one-state recipe should stay as a one-element list
-    one_item_list = Recipe('[{"op":"from morse code", "args": {"wordDelimiter": "Colon"}}]')
+    one_item_list = Recipe(
+        '[{"op":"from morse code", "args": {"wordDelimiter": "Colon"}}]')
     assert len(one_item_list) == 1
 
     # Single stage recipe should become a one-element list
-    one_item = Recipe('{"op":"from morse code", "args": {"wordDelimiter": "Colon"}}')
+    one_item = Recipe(
+        '{"op":"from morse code", "args": {"wordDelimiter": "Colon"}}')
     assert len(one_item) == 1
